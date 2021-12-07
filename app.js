5f60c2a679593fa82476003b3cb88d33a8f85b45
natalia <n.miroslaw93@gmail.com>
2021-12-07 20:30:38 +0100

    feat (app.js): add place mark function

---
+++ app.js
@@ -0,0 +1,19 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
+const cellElements = document.querySelectorAll('[data-cell]');
+let circleTurn;
+
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+  //Check for win
+  //check for draw
+  //switch turns
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+cellElements.forEach(cell => {cell.addEventListener('click', handleClick, {once: true})});
