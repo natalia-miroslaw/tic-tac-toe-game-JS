c7ad2b512708a0a8893a8cede16fadae586379ff
natalia <n.miroslaw93@gmail.com>
2021-12-07 22:42:48 +0100

    feat (app.js): add winning message function

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
 let circleTurn;
 
 function startGame() {
@@ -19,9 +31,21 @@ function handleClick(e) {
   placeMark(cell, currentClass);
   //Check for win
   //check for draw
+  if (checkWin(currentClass)) {
+    endGame(false);
+  }
   //switch turns
   swapTurns();
   setBoardHoverClass();
+}
+
+function endGame(draw) {
+  if (draw) {
+
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O\'s" : "X\'s"} Wins!`
+  }
+  winningMessageElement.classList.add('show');
 }
 
 function placeMark(cell, currentClass) {
@@ -42,3 +66,10 @@ function setBoardHoverClass() {
   }
 }
 
+function checkWin(currentClass) {
+   return WINNING_COMBINATIONS.some(combination => {
+     return combination.every(index => {
+       return cellElements[index].classList.contains(currentClass)
+     });
+   });
+}
